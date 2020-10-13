e924d7724d68d98f7c1b03b0868ad7131add4206
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2020-10-13 04:42:34 +0200

    update

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    dbUrl: 'mongodb+srv://alex:qwe@cluster0.euvd6.gcp.mongodb.net/twitter?retryWrites=true&w=majority',
+    dbUrl: 'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
     cert: '/etc/letsencrypt/live/www.rodrigue-projects.site/fullchain.pem',
     key: '/etc/letsencrypt/live/www.rodrigue-projects.site/privkey.pem'
 }
