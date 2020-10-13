29f00421778a0a5cbec1f914ab0255ad379769fc
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2020-10-13 02:45:36 +0200

    firs commit

---
+++ environment/production.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+    dbUrl: 'mongodb+srv://alex:qwe@cluster0.euvd6.gcp.mongodb.net/twitter?retryWrites=true',
+    cert: path.join(__dirname, ''),
+    key: path.join(__dirname, '')
+}
