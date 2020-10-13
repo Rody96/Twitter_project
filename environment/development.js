e169a98ddcdee1e4d0d44b93f0a78107c13a4dd9
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2020-10-13 04:37:44 +0200

    update

--- environment/development.js
+++ environment/development.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    dbUrl: 'mongodb+srv://alex:qwe@cluster0.euvd6.gcp.mongodb.net/twitter?retryWrites=true&w=majority',
+    dbUrl: 'mongodb+srv://alex:qwe@cluster0.euvd6.gcp.mongodb.net/twitter?retryWrites=true',
     cert: path.join(__dirname, '../ssl/local.crt'),
     key: path.join(__dirname, '../ssl/local.key')
 }
