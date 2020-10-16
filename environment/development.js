66e310003a96274cf3ed90dc4ddc5717ded88e1d
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2020-10-16 21:11:13 +0200

    update

--- environment/development.js
+++ environment/development.js
@@ -3,5 +3,7 @@ const path = require('path');
 module.exports = {
     dbUrl: 'mongodb+srv://alex:qwe@cluster0.euvd6.gcp.mongodb.net/twitter?retryWrites=true&w=majority',
     cert: path.join(__dirname, '../ssl/local.crt'),
-    key: path.join(__dirname, '../ssl/local.key')
+    key: path.join(__dirname, '../ssl/local.key'),
+    portHttp: 3000,
+    portHttps: 3001
 }
--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const { porHttp } = require('./development');
 
 module.exports = {
     dbUrl: 'mongodb+srv://alex:qwe@cluster0.euvd6.gcp.mongodb.net/twitter?retryWrites=true&w=majority',
     cert: '/etc/letsencrypt/live/www.rodrigue-projects.site/fullchain.pem',
-    key: '/etc/letsencrypt/live/www.rodrigue-projects.site/privkey.pem'
+    key: '/etc/letsencrypt/live/www.rodrigue-projects.site/privkey.pem',
+    portHttp: 80,
+    portHttps: 443
 }
